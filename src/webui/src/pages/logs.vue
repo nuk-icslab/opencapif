977c7c8dab831aac5ca42417ca0d3474242ff9cc
Yung-Hsiang Hu <iftnt1999@gmail.com>
2022-04-11 04:22:41 +0800

    webui: add support for querying logs

--- src/webui/src/pages/logs.vue
+++ src/webui/src/pages/logs.vue
@@ -2,13 +2,13 @@
   <div>
     <div class="row justify-content-between mb-3">
       <div class="col-5">
-        <input type="text" class="form-control" v-model="apf_id" placeholder="AEF ID" />
+        <input type="text" class="form-control" v-model="aef_id" placeholder="AEF ID" />
       </div>
       <div class="col-5">
         <input type="text" class="form-control" v-model="invkr_id" placeholder="Invoker ID" />
       </div>
       <div class="col-1">
-        <button class="btn btn-primary" type="button" @click="queryApf">Query</button>
+        <button class="btn btn-primary" type="button" @click="queryLog">Query</button>
       </div>
     </div>
     <table class="table table-striped table-sm table-responsive">
@@ -18,16 +18,18 @@
           <th scope="col">API Name</th>
           <th scope="col">Version</th>
           <th scope="col">Resource</th>
-          <th scope="col">Operation</th>
+          <!-- <th scope="col">Operation</th> -->
           <th scope="col">Result</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="log in logs">
-          <td>{{ log.name }}</td>
-          <td>{{ api.aef_id }}</td>
-          <td>{{ api.versions.join(', ') }}</td>
-          <td>{{ api.domain }}</td>
+          <td>{{ log.time }}</td>
+          <td>{{ log.api_name }}</td>
+          <td>{{ log.version }}</td>
+          <td>{{ log.resource }}</td>
+          <!-- <td>{{ log.operation }}</td> -->
+          <td>{{ log.result }}</td>
         </tr>
       </tbody>
     </table>
@@ -35,15 +37,53 @@
 </template>
 
 <script>
+const { ccfUrl } = require('../../config/ccf.js');
+const capifWebUiClient = require('capif-webui-client');
+const { ApiClient, DefaultApi, ServiceAPIDescription } = capifWebUiClient;
 export default {
   name: "logs",
   data() {
     return {
       aef_id: "",
-      invkr_id: ""
+      invkr_id: "",
+      logs: []
     };
   },
   methods: {
+    queryLog: function () {
+      const my_api_client = new ApiClient(ccfUrl);
+      const my_default_api = new DefaultApi(my_api_client);
+
+      let opts = {};
+
+      if (this.aef_id !== "") {
+        opts['aefId'] = this.aef_id;
+      }
+      if (this.invkr_id !== "") {
+        opts['apiInvokerId'] = this.invkr_id;
+      }
+
+      let callback = (error, data) => {
+        if (error) {
+          console.error(error);
+        } else {
+          this.logs = data['logs'].map(log => {
+            let time = new Date(log['invocationTime']);
+            console.log(typeof (time));
+            //console.log(time.toISOString());
+            return {
+              time,
+              api_name: log['apiName'],
+              version: log['apiVersion'],
+              resource: log['resourceName'],
+              operation: log['operation'],
+              resulr: log['result']
+            }
+          })
+        }
+      };
+      my_default_api.logsV1ApiInvocationLogsGet(opts, callback);
+    }
   },
 };
 </script>
