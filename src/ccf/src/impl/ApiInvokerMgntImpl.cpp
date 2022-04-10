7ecbd8d86ccfc56de328f9b84738b1f1a4add364
Yung-Hsiang Hu <iftnt1999@gmail.com>
2022-04-11 00:37:39 +0800

    ccf: add onboarding implimentation

--- src/ccf/src/impl/ApiInvokerMgntImpl.cpp
+++ src/ccf/src/impl/ApiInvokerMgntImpl.cpp
@@ -1,4 +1,5 @@
 #include "DefaultApiImpl.h"
+#include "util.h"
 #include <fmt/core.h>
 #include <spdlog/spdlog.h>
 
@@ -18,6 +19,9 @@ namespace org::openapitools::server::api
 {
 
     using namespace org::openapitools::server::model;
+
+    const std::string API_ROOT = "http://localhost:8080";
+
     void DefaultApiImpl::api_invoker_management_v1_onboarded_invokers_onboarding_id_delete(const std::string &onboardingId, Pistache::Http::ResponseWriter &response)
     {
         response.send(Pistache::Http::Code::Not_Implemented, "Not Implemented\n");
@@ -28,6 +32,29 @@ namespace org::openapitools::server::api
     }
     void DefaultApiImpl::api_invoker_management_v1_onboarded_invokers_post(const APIInvokerEnrolmentDetails &aPIInvokerEnrolmentDetails, Pistache::Http::ResponseWriter &response)
     {
-        response.send(Pistache::Http::Code::Not_Implemented, "Not Implemented\n");
+        APIInvokerEnrolmentDetails res;
+        nlohmann::json json;
+        std::string public_key, invkr_id, new_resource_location;
+
+        public_key = aPIInvokerEnrolmentDetails.getOnboardingInformation().getApiInvokerPublicKey();
+        to_json(json, aPIInvokerEnrolmentDetails);
+
+        spdlog::info("Received an Onboarding request:");
+        spdlog::info("  data={}", json.dump());
+
+        invkr_id = util::uuid::generate_uuid_v4();
+
+        res.setOnboardingInformation(aPIInvokerEnrolmentDetails.getOnboardingInformation());
+        res.setNotificationDestination(aPIInvokerEnrolmentDetails.getNotificationDestination());
+        res.setApiInvokerId(invkr_id);
+        new_resource_location = fmt::format(
+            "{}/api-invoker-management/v1/onboardedInvokers/{}", API_ROOT, invkr_id);
+
+        to_json(json, res);
+
+        response.headers().add<Pistache::Http::Header::Location>(new_resource_location);
+        response.headers().add<Pistache::Http::Header::AccessControlAllowOrigin>("*");
+        response.headers().add<Pistache::Http::Header::ContentType>(MIME(Application, Json));
+        response.send(Pistache::Http::Code::Created, json.dump());
     }
 }
--- src/ccf/src/impl/util.h
+++ src/ccf/src/impl/util.h
@@ -8,5 +8,9 @@ namespace util
 {
     extern bsoncxx::document::value njson2bsoncxx(nlohmann::json njson_obj);
     extern nlohmann::json bsoncxx2njson(bsoncxx::document::view bson_obj);
+    namespace uuid
+    {
+        extern std::string generate_uuid_v4();
+    }
 } // namespace util
 #endif
